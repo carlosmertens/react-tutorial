85eef02a31d9591f778994af7c4a1481ec97820d
Carlos Mertens <mertenscarlos@yahoo.com>
2020-04-16 11:25:02 +0200

    Pass data information with props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import Table from './Table'
 
 class App extends Component {
     render() {
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Actrees',
+            },
+            {
+                name: 'Dennis',
+                job: 'Basrtender',
+            },
+        ]
+
         return (
             <div className="container">
-                <Table />
+                <Table characterData={characters} />
             </div>
         )
     }
