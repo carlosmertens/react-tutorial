c0c433e078a9c51dbd1393fdeffecd3941a24d36
Carlos Mertens <mertenscarlos@yahoo.com>
2020-04-16 23:48:14 +0200

    Format tab space on files

--- src/App.js
+++ src/App.js
@@ -3,36 +3,36 @@ import Table from './Table'
 import Form from './Form'
 
 class App extends Component {
-    state = {
-        characters: [],
-    }
+	state = {
+		characters: [],
+	}
 
-    removeCharacter = index => {
-        const { characters } = this.state
+	removeCharacter = index => {
+		const { characters } = this.state
 
-        this.setState({
-            characters: characters.filter((character, i) => {
-                return i !== index
-            }),
-        })
-    }
+		this.setState({
+			characters: characters.filter((character, i) => {
+				return i !== index
+			}),
+		})
+	}
 
-    handleSubmit = character => {
-        this.setState({ characters: [...this.state.characters, character] })
-    }
+	handleSubmit = character => {
+		this.setState({ characters: [ ...this.state.characters, character ] })
+	}
 
-    render() {
-        const { characters } = this.state
+	render() {
+		const { characters } = this.state
 
-        return (
-            <div className="container">
-                <Table characterData={characters} removeCharacter={
-                    this.removeCharacter
-                } />
-                <Form handleSubmit={this.handleSubmit} />
-            </div>
-        )
-    }
+		return (
+			<div className="container">
+				<Table characterData={ characters } removeCharacter={
+					this.removeCharacter
+				} />
+				<Form handleSubmit={ this.handleSubmit } />
+			</div>
+		)
+	}
 }
 
 export default App
