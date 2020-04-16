b050c1689ba69781547fc6c6eda50a40118e1fc5
Carlos Mertens <mertenscarlos@yahoo.com>
2020-04-16 16:11:41 +0200

    Updates

---
+++ precache-manifest.93e8c480e086f0356c940e2e8c4dc57d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e76e8aeb803aea284a6be4ac722d39",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "c91df31cc87255f40986",
+    "url": "/react-tutorial/static/css/main.db2fbb6c.chunk.css"
+  },
+  {
+    "revision": "607c18d14458d18821eb",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91df31cc87255f40986",
+    "url": "/react-tutorial/static/js/main.288629c3.chunk.js"
+  },
+  {
+    "revision": "32d96f51efef0a2c7187",
+    "url": "/react-tutorial/static/js/runtime-main.079d628d.js"
+  }
+]);
