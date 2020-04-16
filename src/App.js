bc88b7120753cdf2d3163291d8697e9b7512e398
Carlos Mertens <mertenscarlos@yahoo.com>
2020-04-16 10:31:29 +0200

    Create Table custom class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Table from './Table'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>Hello, React! Welcome Components...</h1>
+            <div className="container">
+                <Table />
             </div>
         )
     }
